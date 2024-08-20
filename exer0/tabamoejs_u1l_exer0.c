97e4e1e0a962c35a885daa50d3af2173f54bbf9f
Euan Jed Tabamo <tabamoeuan@gmail.com>
2024-08-20 22:53:59 +0800

    chore: Documentation and clean up of main() function

--- exer0/tabamoejs_u1l_exer0.c
+++ exer0/tabamoejs_u1l_exer0.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Node Structure Definition
 typedef struct node_tag
 {
     int value;
@@ -22,17 +23,21 @@ typedef struct node_tag
 */
 NODE *createNode(int data)
 {
+    // Allocate memory for a node
     NODE *ptr = (NODE *)malloc(sizeof(NODE));
+    // If memory allocation failed, exit the program with EXIT_FAILURE status
     if (ptr == NULL)
     {
         printf("ERROR: Memory allocation has failed.\n");
         exit(EXIT_FAILURE);
     }
+    // Otherwise, initialize the fields of the node
     else
     {
         ptr->value = data;
         ptr->next = NULL;
     }
+    // Return the created node
     return ptr;
 }
 
@@ -44,6 +49,7 @@ NODE *createNode(int data)
 */
 int isEmpty(NODE *head)
 {
+    // If the head is NULL, the list is empty
     return head == NULL;
 }
 
@@ -55,6 +61,7 @@ int isEmpty(NODE *head)
 */
 void insert(NODE **head, int value)
 {
+    // Create a new node, then prepend it to the given list
     NODE *newNode = createNode(value);
     newNode->next = *head;
     *head = newNode;
@@ -69,12 +76,17 @@ void insert(NODE **head, int value)
 */
 int delete(NODE **head)
 {
+    // If the list is empty, print an error message and return -1 (sentinel value)
     if (*head == NULL)
     {
         printf("ERROR: Linked List is empty.\n");
         return -1; // Return a -1 for an 'absence' of value in this case.
     }
 
+    // Otherwise, get the current head's value,
+    // then update the head pointer to the next node
+    // then free the memory of the previous head node
+    // then return the value of the previous head
     NODE *temp = *head;
     int value = temp->value;
     *head = temp->next;
@@ -91,11 +103,14 @@ int delete(NODE **head)
 void printList(NODE *head)
 {
     NODE *current = head;
+    // If the list is empty, print an empty list
     if (current == NULL)
     {
         printf("[]\n");
         return;
     }
+
+    // Otherwise, print the list in the format: [<start>, ..., <end>]
     printf("[");
     while (current != NULL)
     {
@@ -111,41 +126,36 @@ void printList(NODE *head)
 int main()
 {
     NODE *head = NULL;
-    // Insertion
+
+    // TEST: Insertion
     insert(&head, 5);
     insert(&head, 4);
     insert(&head, 3);
     insert(&head, 2);
     insert(&head, 1);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > [1, 2, 3, 4, 5]
 
-    // Deletion
+    // TEST: Deletion
     delete (&head);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > [2, 3, 4, 5]
     delete (&head);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > [3, 4, 5]
     delete (&head);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > [4, 5]
     delete (&head);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > [5]
     delete (&head);
-    printList(head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printList(head); // > []
 
-    // Deletion on an empty list
-    delete (&head);
-    printList(head);
+    // TEST: Deletion on an empty list
+    delete (&head);  // > ERROR: Linked List is empty.
+    printList(head); // > []
 
-    // isEmpty() on a filled list and an empty list
+    // TEST: isEmpty() on a filled list and an empty list
     insert(&head, 100);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n")); // > The list is not empty.
     delete (&head);
-    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n"));
+    printf((isEmpty(head) ? "The list is empty.\n" : "The list is not empty.\n")); // > The list is empty.
 
     // Free memory
     while (head != NULL)
